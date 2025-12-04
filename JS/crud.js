eba2268f2e0fd88385c60fcbaad32e58c3af7707
anushka26bangar <bangaranushka09@gmail.com>
2025-12-04 20:46:27 +0530

    Initial Commit

---
+++ JS/crud.js
@@ -0,0 +1,30 @@
+import { url } from "./metadata.js";
+
+const nameInput = document.getElementById("name");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("list");
+
+addBtn.onclick = function () {
+  const n = nameInput.value.trim();
+
+  fetch(`${url}.json`, {
+    method: "POST",
+    body: JSON.stringify({ n })
+  }).then(() => {
+    nameInput.value = "";
+    loadData();
+  });
+};
+
+function loadData() {
+  fetch(`${url}.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      list.innerHTML = "";
+      for (let key in data) {
+        list.innerHTML += `<li>${data[key].n}</li>`;
+      }
+    });
+}
+
+loadData();
---
+++ JS/metadata.js
@@ -0,0 +1,2 @@
+export const url =
+  "https://mini-fullstack-app-bd253-default-rtdb.firebaseio.com/";
